dba6083bfaefeaffd555012e9fcfd458d69393c0
dorvaan <104641899+dorvaan@users.noreply.github.com>
2022-06-10 09:25:25 -0500

    Calculations w/o decimal are working

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,37 @@
 "use strict";
 
-let firstOperand = 82;
-let secondOperand = 84;
-let operator = "+";
-let displayValue = "55";
+let firstOperand = "";
+let secondOperand = "";
+let operator = "";
+let displayValue = "";
 let calcDisplay = document.getElementById("output");
 let grandTotal = "";
 
 const clearButton = document.getElementById("btnClear");
 const numberButtons = document.querySelectorAll(".btnNumber");
+const operatorButtons = document.querySelectorAll(".btnOperator");
 
 numberButtons.forEach((button) =>
   button.addEventListener("click", () => {
     appendNumber(button.textContent);
   })
 );
+
+operatorButtons.forEach((button) =>
+  button.addEventListener("click", () => {
+    appendOperator(button.textContent);
+  })
+);
+
+document.getElementById("btnEqual").addEventListener("click", () => {
+  secondOperand = displayValue;
+  console.log(`First Operand ${firstOperand}`);
+  console.log(`Second Operand ${secondOperand}`);
+  console.log(`Operator ${operator}`);
+  displayValue = "";
+  operate(operator);
+  console.log();
+});
 
 function appendNumber(number) {
   displayValue += number;
@@ -23,7 +40,34 @@ function appendNumber(number) {
   return displayValue;
 }
 
+function appendOperator(calcOperator) {
+  firstOperand = displayValue;
+  operator = calcOperator;
+  calcDisplay.innerHTML = `${displayValue} ${operator}`;
+  displayValue = "";
+  console.log(`First Operand: ${firstOperand}`);
+  console.log(firstOperand);
+  console.log(`Operator: ${operator}`);
+  console.log(`DisplayValue: ${displayValue}`);
+}
+
 clearButton.addEventListener("click", clear);
+
+//Function to call the Calculator operations
+function operate(operator) {
+  if (operator == "+") {
+    calcDisplay.innerHTML = addTotal(
+      number(firstOperand),
+      number(secondOperand)
+    );
+  } else if (operator === "-") {
+    calcDisplay.innerHTML = subTotal(firstOperand, secondOperand);
+  } else if (operator === "x") {
+    calcDisplay.innerHTML = multTotal(firstOperand, secondOperand);
+  } else if (operator === "÷") {
+    calcDisplay.innerHTML = divTotal(firstOperand, secondOperand);
+  }
+}
 
 //Calculator Operations
 const addTotal = (firstOperand, secondOperand) => {
@@ -45,19 +89,6 @@ const divTotal = (firstOperand, secondOperand) => {
   let a = firstOperand / secondOperand;
   return a;
 };
-
-//Function to call the Calculator operations
-function operate(operator) {
-  if (operator == "+") {
-    calcDisplay.innerHTML = addTotal(firstOperand, secondOperand);
-  } else if (operator === "-") {
-    calcDisplay.innerHTML = subTotal(firstOperand, secondOperand);
-  } else if (operator === "*") {
-    calcDisplay.innerHTML = multTotal(firstOperand, secondOperand);
-  } else if (operator === "/") {
-    calcDisplay.innerHTML = divTotal(firstOperand, secondOperand);
-  }
-}
 
 //Clear Screen
 function clear() {
